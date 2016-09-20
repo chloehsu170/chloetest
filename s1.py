23ffb3f53745d0bde2eacd98e9028eedc97262f5
chloehsu170 <xuyanwen170@gmail.com>
2016-09-20 11:16:11 +0800

    loginSpm

--- s1.py
+++ s1.py
@@ -2,8 +2,13 @@ __author__ = 'chloe'
 #coding=utf-8
 #import selenium print selenium.__file__
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get("http://www.baidu.com")
-browser.find_element_by_id("kw").send_keys("selenium")
-browser.find_element_by_id("su").click()
+import time
+browser = webdriver.Chrome()
+browser.get("http://10.10.10.46:8080/sse-spm/sys/user/logout.html")
+browser.implicitly_wait(10)
+browser.find_element_by_id("userName").send_keys("xuyanwen")
+browser.find_element_by_id("password").send_keys("pass")
+browser.find_element_by_id("LoginBtn").click()
+browser.implicitly_wait(10)
+browser.find_element_by_xpath(".//*[@id='signstar_wrapper']/a[1]").click()
 browser.quit()
